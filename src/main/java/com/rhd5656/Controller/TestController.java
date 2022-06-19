d166d183da64487a03701f053350d28b1a59a2bd
rhd5656 <rhd5656@gmail.com>
2022-06-19 19:41:34 +0900

    add log4j2

--- src/main/java/com/rhd5656/Controller/TestController.java
+++ src/main/java/com/rhd5656/Controller/TestController.java
@@ -1,13 +1,21 @@
 package com.rhd5656.Controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TestController {
-
-    @RequestMapping("/")
+    private static Logger LOGGER = LogManager.getLogger(TestController.class);
+    @RequestMapping("/test")
     public String test_home() {
+        LOGGER.info("LOGGER info");
+        LOGGER.debug("LOGGER debug");
+        LOGGER.error("LOGGER error");
+        LOGGER.warn("LOGGER warn");
+        LOGGER.trace("LOGGER trace");
         return "index";
     }
 }
